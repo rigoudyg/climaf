99d9fc99f241f80c345d1de5441ac0e214778dfa
Stephane Senesi <stephane.senesi@meteo.fr>
2015-04-10 09:03:46 +0200

    Forgot some new examples

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode']
-#     'sphinx.ext.napoleon'
+     'sphinx.ext.napoleon'
 #    'sphinxcontrib.napoleon',
 
 # Add any paths that contain templates here, relative to this directory.
