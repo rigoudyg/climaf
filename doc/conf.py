8e9fc065dfee7425f6960424fcf795c1076b0be0
Stéphane Sénési <senesi@meteo.fr>
2015-04-16 17:59:51 +0200

    Fix conf.py about napoleon extension for ReadTheDocs

--- doc/conf.py
+++ doc/conf.py
@@ -31,12 +31,12 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.autodoc'
+    ,'sphinx.ext.todo'
+    ,'sphinx.ext.viewcode'
+    ,'sphinx.ext.napoleon' #this one works at RTD
+    #,'sphinxcontrib.napoleon' #This one works at CNRM
 ]
-#     'sphinx.ext.napoleon'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
