345cbce3f48f912ad6a750bf5da5017274c80382
Stephane Senesi <senesi@meteo.fr>
2015-05-26 15:46:09 +0200

    Fix version number. Add 'enesmeble.py' to tests on Ciclad

--- testing/test_examples.sh
+++ testing/test_examples.sh
@@ -12,12 +12,12 @@ cd $dir/examples
 
 # List of scripts that can be tested anywhere (based on data installed with the package, or a test is done)
 scripts="clean_cache.py data_generic.py plotmap.py basic_oce.py latlonbox.py ann_cycle.py derived.py \
-         export.py increm.py regrid.py latlonbox.py macro.py plot_timeseries.py plot_xsection.py "
+         export.py increm.py regrid.py latlonbox.py macro.py plot_timeseries.py plot_xsection.py figarray.py "
 
 # Add some scripts, depending on the data available at each site
 if [[ $(uname -n) == lx* || $(uname -n) == sx* ]]; then 
     # CNRM
-    scripts=$scripts" data_cmip5drs.py figarray.py ensemble.py data_obs.py"
+    scripts=$scripts" data_cmip5drs.py ensemble.py data_obs.py"
     # Add scripts depending on user-configured data
     [ $(whoami) = senesi ] && scripts=$scripts" data_em.py seaice.py"
 elif [[ $(uname -n) == ciclad* ]]; then 
