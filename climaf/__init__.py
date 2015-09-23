ae10bd8cea3739e997bfe6221e29d9c4bf20bf4c
Stéphane Sénési <senesi@meteo.fr>
2015-09-23 08:29:28 +0200

    Try to avoid import issue at RTFD

--- climaf/__init__.py
+++ climaf/__init__.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 
 # Created : S.Senesi - 2014
 
-__all__=[ "site_settings", "cache" , "classes", "clogging", "dataloc", "driver", "netcdfbasics",
+__all__=[ "site_settings", "cache" , "classes", "clogging", "dataloc", "driver", 
           "operators", "period", "standard_operators" , "projects" ,"cmacro","html"]
 
 version="0.9"
