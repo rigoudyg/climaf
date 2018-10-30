70fdddc014adb4549ae570ab670ed604f526b574
Stéphane Sénési <senesi@meteo.fr>
2018-10-30 16:28:23 +0100

    mcdo.sh use 'cdo -mergetime' rather than concatenating by itself

--- scripts/mcdo.sh
+++ scripts/mcdo.sh
@@ -47,9 +47,7 @@ if [ "$region" ] ; then
     selregion="-sellonlatbox,$lonmin,$lonmax,$latmin,$latmax"
 fi
 
-if [ "$units" ] ; then
-    setunits="-setattribute,"${var}"@units="${units}
-fi
+[ "$units" ] && setunits="-setattribute,"${var}"@units="${units} 
 
 if [ "$alias" ] ; then 
     IFS=", " read var filevar scale offset <<< $alias 
@@ -58,85 +56,42 @@ else
     filevar=$var
 fi
 
-if [ "$vm" ] ; then 
-    setmiss="-setctomiss,"$vm
-fi
-
+[ "$vm" ] && setmiss="-setctomiss,"$vm 
 
 # 'files' is the list of input filenames
 files=$*
-single_file=no ; [ $(echo $files | wc -w) -eq 1 ] && single_file=yes
+
+# prepare to invoke CDO operator 'mergetime' if needed
+merge="" ; [ $(echo $files | wc -w) -gt 1 ] && merge="-mergetime"
+
+# If needed, transform date selection command in (time fix + date selection)
+ffile=$(echo $files | cut -d " " -f 1)
+seldate=$seldatebase
+[ "$seldate" ] && seldate=$seldatebase" "$(clim_timefix $ffile) 
+
+# Prepare all selections (setmiss, aliasing, time, space, variable) :
+# construct CDO operators chain, optimizing operations list and order
+if [[ $ffile == *${filevar}_* ]] ; then 
+    # varname is in filename -> assume seldate before selvar is best
+    ops=$setmiss" "$setunits" "$selalias" "$selvar" "$seldate" "$selregion" "$merge
+else
+    # varname is not in filename -> assume selvar before selvar is best
+    ops=$setmiss" "$setunits" "$selalias" "$seldate" "$selvar" "$selregion" "$merge
+fi
+[ $operator ] && ops="-"${operator}" "$ops 
+#
+
+# Construct files list, which may involve transformed files
 vfiles=""
-vfiles_are_original=0
-
-# Make all selections (setmiss, aliasing, time, space, variable) 
-# before applying the operator
-# however, operator si also applied here if there is only one file
-
 for file in $files ; do
-    tmp2=$tmp/$(basename $file) ; rm -f $tmp2
-    # If needed, transform date selection command in (time fix + date selection)
-    seldate=$seldatebase
-    [ "$seldate" ] && seldate=$seldatebase" "$(clim_timefix $file) 
-    #
     # If requested, fix time_counter for Nemo outputs. Feature not yet tested !!
     [ "${CLIMAF_FIX_NEMO_TIME:-no}" != no ] && file=$(nemo_timefix $file) 
     # If requested, fix attribute 'coordinates' of file variable for Aladin outputs.
     [ "${CLIMAF_FIX_ALADIN_COORD:-no}" != no ] && file=$(aladin_coordfix $file)
     #
-    # Construct CDO operators chain as needed, optimizing operations list and order
-    if [[ $file == *${filevar}_* ]] ; then 
-	# varname is in filename -> assume seldate before selvar is best
-	ops=$setmiss" "$setunits" "$selalias" "$selvar" "$seldate" "$selregion
-    else
-	# varname is not in filename -> assume selvar before selvar is best
-	ops=$setmiss" "$setunits" "$selalias" "$seldate" "$selvar" "$selregion
-    fi
-    #
-    if [ "${ops// /}" ] ; then
-	# There is some operator to apply
-	[ $single_file = yes ] && [ $operator ] && ops="-"${operator}" "$ops 
-	$CDO $ops $file $tmp2 && [ -f $tmp2 ] && vfiles+=" "$tmp2
-    else
-	# Just keep the file as is
-	vfiles+=" "$file ; vfiles_are_original=1
-    fi
+    vfiles+=" "$file
 done
 
-
-if [ "$vfiles" ] ; then
-    if [ $single_file = yes ] ; then
-	file=$vfiles
-	# Operator was applied upstream (if one is defined )
-	if [ $vfiles_are_original -eq 1 ]; then cp $file $out ; else mv $file $out ; fi
-    else
-	tmp3=$tmp/$(basename $0).nc ; rm -f $tmp3
-	#
-	if [ ${timelinear:-no} == no ] ; then 
-	    # Must assemble all datafiles in a single one before applying 
-	    # the operator if it is non-linear in time - e.g. eigenvectors 
-            # (but avoid single file copy followed by rm ...)
-	    if [ $(echo $vfiles | wc -w) -gt 1 ] ; then 
-		cdo copy $vfiles $tmp3 && [ $vfiles_are_original -eq 0 ] && rm $vfiles 
-	    else  # Only one file to process
-		file=$vfiles
-		if [ $vfiles_are_original -eq 1 ]
-		then cp $file $tmp3 ; 
-		else mv $file $tmp3 ; fi
-	    fi
-            #
-            # Apply operator if requested
-	    if [ "$operator" ] ; then 
-		cdo $operator $tmp3 $out && rm $tmp3 || exit 1
-	    else mv $tmp3 $out ; fi
-	else
-	    cdo $operator $vfiles $out && [ $vfiles_are_original -eq 0 ] && rm $vfiles 
-	fi
-    fi
-else
-    echo "Issue while selecting data from $files ">&2
-    rm -fr $tmp
-    exit 1
-fi
+time $CDO $ops "$vfiles" $out
 
 rm -fr $tmp
