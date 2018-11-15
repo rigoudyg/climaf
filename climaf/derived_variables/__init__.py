631ff27af7be58649031d448c8fac082aa978e7d
Stéphane Sénési <senesi@meteo.fr>
2018-11-15 15:54:00 +0100

    Don't systematically import atmosphere_derived_variables (side effects..)

--- climaf/derived_variables/__init__.py
+++ climaf/derived_variables/__init__.py
@@ -1,5 +1,7 @@
 
-__all__=[ "atmosphere_derived_variables", "ocean_derived_variables" ]
+# Some projects like atmosphere_derived_variables, others don't
+#__all__=[ "atmosphere_derived_variables", "ocean_derived_variables" ]
+__all__=[ "ocean_derived_variables" ]
 
 from climaf.site_settings import atIPSL
 # -- Load only the ipsl derived variables if we are at IPSL
