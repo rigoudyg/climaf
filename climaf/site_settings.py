f66ee2c7391877c61ed9962d3361a008519ddecf
jerome servonnat <jservon@ciclad-ng.private.ipsl.fr>
2020-04-29 10:14:20 +0200

    New functions in functions.py; correction in ensemble_time_series_plot.py; gplot.ncl is compliant with MEDCORDEX files

--- climaf/site_settings.py
+++ climaf/site_settings.py
@@ -29,7 +29,7 @@ if os.path.exists('/ccc') and not os.path.exists('/data'):
     atIPSL = True
 if os.path.exists('/cnrm'):
     atCNRM = True
-if 'ciclad' in HostName or 'loholt' in HostName or 'ipsl.polytechnique.fr' in HostName:
+if 'ciclad' in HostName or 'loholt' in HostName or 'ipsl.polytechnique.fr' in HostName or 'climserv' in HostName:
     onCiclad = True
     atIPSL = True
 if 'ada' in HostName:
