043d6284b691a17562f2eeb774a9376c685be726
Stéphane Sénési <senesi@posteo.net>
2022-06-23 11:45:17 +0200

    Fix pattern for project igcm_out at TGCC (discard segment '/IGCM_OUT')

--- climaf/projects/igcm_out.py
+++ climaf/projects/igcm_out.py
@@ -178,7 +178,8 @@ if root:
         ]
     if atTGCC:
         urls_IGCM_OUT = [
-            "${root}/${login}/IGCM_OUT/${model}/${status}/${experiment}/${simulation}/${DIR}/${OUT}/${ave_length}/"
+            #"${root}/${login}/IGCM_OUT/${model}/${status}/${experiment}/${simulation}/${DIR}/${OUT}/${ave_length}/"
+            "${root}/${login}/${model}/${status}/${experiment}/${simulation}/${DIR}/${OUT}/${ave_length}/"
             "${simulation}_${PERIOD}_${frequency}_${variable}.nc",
             "${root}/${login}/IGCM_OUT/${model}/${status}/${experiment}/${simulation}/${DIR}/${OUT}/"
             "${frequency}${clim_period_length}/${simulation}_${frequency}_${clim_period}_1M_${variable}.nc",
