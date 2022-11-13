c1912ffd72b3208913530ed76b47327926daa425
Stéphane Sénési <senesi@posteo.net>
2022-11-13 09:18:14 +0100

    setup.py imports climaf_version from env.environment

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = __import__('climaf.cache').climaf_version
+#version = __import__('climaf.cache').climaf_version
+version = __import__('env.environment').climaf_version
 description = 'CliMAF: a Climate Model Assessment Framework.'
 long_description = (
     open('README.rst').read()
